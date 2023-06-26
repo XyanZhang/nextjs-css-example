927e1a297692d984e9b189f00703b87012ba3a5c
XyanZhang <1743067175@qq.com>
2023-06-26 10:49:53 +0800

    style: 侧边栏响应式动画

--- src/app/background/layout.tsx
+++ src/app/background/layout.tsx
@@ -1,16 +1,18 @@
 import { Navigation } from "@/components/navigation";
 import { navList } from "@/config/nav.config";
+import styles from './layout.module.css'
+
 export default function BackgroundLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode
 }) {
   return (
-      <section className="flex flex-col md:flex-row">
-        <div className="w-full md:w-1/6">
+      <section className="flex flex-row">
+        <div className={`w-200px max-md:w-0 overflow-hidden ${styles.asideTransition}`}>
           <Navigation navLinks={navList}></Navigation>
         </div>
-        <div className="w-full md:w-3/4">
+        <div className="flex-1">
           {children}
         </div>
       </section>
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -4,24 +4,25 @@ import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 
 export function Navigation({ navLinks }: { navLinks: any[] }) {
-  const pathname = usePathname()
- 
   return (
-    <>
     <div className="flex flex-col h-screen bg-gray-200">
       {navLinks.map((link) => {
-        const isActive = pathname.startsWith(link.url)
-        return (
-          <Link
-            className={isActive ? 'text-red-800' : 'text-black'}
-            href={link.url}
-            key={link.title}
-          >
-            {link.title}
-          </Link>
-        )
+        return <NavItem link={link}></NavItem>
       })}
-      </div>
-    </>
+    </div>
+  )
+}
+
+function NavItem({ link }: { link: any}) {
+  const pathname = usePathname()
+  const isActive = pathname.startsWith(link.url)
+  return (
+    <Link
+      className={`${isActive ? 'text-red-800' : 'text-black'} m-0.5 block py-2 .m-0\.5 rounded-2 text-center font-semibold truncate`}
+      href={link.url}
+      key={link.title}
+    >
+      {link.title}
+    </Link>
   )
 }
