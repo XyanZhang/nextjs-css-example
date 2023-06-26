62475807af988cad88c5f29e4fdacd8de8c0c792
XyanZhang <1743067175@qq.com>
2023-06-26 20:59:41 +0800

    feat: tailwind gradient使用

--- note/tailwind.use.md
+++ note/tailwind.use.md
@@ -7,3 +7,11 @@
 ## 响应式
 
 **在需要设置响应式的class中 `[lg|md|sm]:classname`,例如`md:grid-cols-2 lg:grid-cols-3`**
+
+### tailwind 使用渐变
+
+source<https://tailwindcss.com/docs/gradient-color-stops>
+
+```css
+<li className="border-2 border-black bg-gradient-to-b from-white from-50%  to-black to-50%"></li>
+```
--- src/app/(items)/background/page.tsx
+++ src/app/(items)/background/page.tsx
@@ -1,11 +1,28 @@
 import { Metadata } from "next"
 import { navList } from "@/config/nav.config"
-
+import styles from './index.module.css'
 let currentNav = navList[0];
 export default function BackgroundCSS(props:any) {
   return (
-    <div className="background">
-      
+    <div className="background p-10">
+      <ul className={styles.base}>
+        <li className="bg-[black]"></li>
+        <li className="bg-gradient-to-b from-white to-black"></li>
+        <li className="bg-gradient-radial from-white to-black"></li>
+        <li className="bg-gradient-conic from-white to-black"></li>
+      </ul>
+      <br />
+      <ul className={styles.base}>
+        <li className="bg-[black]"></li>
+        {/* border: 2px solid #000; background: linear-gradient(#fff 0%, #fff 50%, #f00 50%, #f00 100%); */}
+        {/* https://tailwindcss.com/docs/gradient-color-stops */}
+        <li className="border-2 border-black bg-gradient-to-b from-white from-50%  to-black to-50%"></li>
+        <li className="border-black bg-gradient-to-br from-white from-50%  to-black to-50%"></li>
+        <li className="border-black bg-gradient-to-br from-transparent from-50%  to-black to-50%"></li>
+        <li className="border-black bg-gradient-to-br from-white from-50%  to-transparent to-50%"></li>
+        <li className="bg-gradient-radial from-white to-black"></li>
+        <li className="bg-gradient-conic from-white to-black"></li>
+      </ul>
     </div>
   )
 }
