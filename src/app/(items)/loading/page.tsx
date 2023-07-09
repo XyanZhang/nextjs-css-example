381670653b745519e9555b4fe0deaf95b7f0801d
XyanZhang <1743067175@qq.com>
2023-07-09 15:32:24 +0800

    feat: add  animatedtxt demo

--- src/app/(items)/loading/page.tsx
+++ src/app/(items)/loading/page.tsx
@@ -1,9 +1,20 @@
+"use client";
 import { Metadata } from "next"
+import { Phrase, Char } from "animatedtxt";
 
 export default function BackgroundCSS() {
   return (
-    <div>
+    <div className="text-center">
       <h1>loading</h1>
+      <Phrase color="var(--theme1-2)" margin={10}>
+        <Char char="L" />
+        <Char char="O" delay={0.5} />
+        <Char char="A" delay={1} />
+        <Char char="D" delay={1.5} />
+        <Char char="I" delay={2} />
+        <Char char="N" delay={2.5} />
+        <Char char="D" delay={3} />
+      </Phrase>
     </div>
   )
 }
