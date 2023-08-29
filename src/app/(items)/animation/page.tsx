327541e543d104dcfacf56bf259b30333b0d3df3
XyanZhang <1743067175@qq.com>
2023-08-29 22:50:49 +0800

    feat: loading circle

--- src/app/(items)/animation/page.tsx
+++ src/app/(items)/animation/page.tsx
@@ -4,7 +4,14 @@ import styles from './index.module.scss'
 
 export default function AnimationCSS() {
   return (
-    <div>    
+    <div>
+      <div className="flex justify-around h-60 items-center">
+        <div className={styles.loadingDelay}>
+          <span className={styles.item}></span>
+          <span className={styles.item}></span>
+          <span className={styles.item}></span>
+        </div>
+      </div>
     </div>
   )
 }
