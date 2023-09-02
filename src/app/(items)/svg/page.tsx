947ff77e7a63f85923ab298ecf05b43d2aa63fa3
XyanZhang <1743067175@qq.com>
2023-09-02 22:39:34 +0800

    feat: stroke-dasharray 和 stroke-dashoffset

--- src/app/(items)/svg/page.tsx
+++ src/app/(items)/svg/page.tsx
@@ -33,6 +33,17 @@ export default function SVGCss() {
           <rect className={styles.rect} />
         </svg>
       </div>
+      <div className={'flex justify-around h-60 items-center ' + styles.svgs}>
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <rect className={styles.rectSp1} />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <rect className={styles.rectSp2} />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <rect className={styles.rectSp3} />
+        </svg>
+      </div>
     </div>
   );
 }
@@ -40,3 +51,8 @@ export default function SVGCss() {
 export const metadata: Metadata = {
   title: 'SVG Css',
 };
+
+// SVG 中，stroke-dasharray 和 stroke-dashoffset。
+
+// stroke-dasharray：值是一组数组，没数量上限，每个数字交替表示边框与间隔的宽度；
+// stroke-dashoffset：表示的是边框线的偏移量。
