3dde5b87b61c374e89c42ebf56ba8729ea123243
XyanZhang <1743067175@qq.com>
2023-11-25 21:54:40 +0800

    feat: transit use

--- src/app/(items)/transition/page.tsx
+++ src/app/(items)/transition/page.tsx
@@ -1,15 +1,30 @@
-
-import { Metadata } from "next"
-import styles from './index.module.scss'
+import { Metadata } from 'next';
+import styles from './index.module.scss';
 
 export default function TransitionCSS() {
   return (
-    <div>    
+    <div>
+      <div className="flex justify-around h-40 items-center">
+        <div className={styles['g-container']}>
+          <div className={styles['g-button']}>
+              Button
+              <ul>
+                  <li>>> Lorem, ipsum dolor sit amet?</li>
+                  <li>>> Lorem, ipsum dolor sit amet?</li>
+                  <li>>> Lorem, ipsum dolor sit amet?</li>
+                  <li>>> Lorem, ipsum dolor sit amet?</li>
+              </ul>
+          </div>
+        </div>
+        <div className={styles['hover-trans']}>按钮hover</div>
+      </div>
+      <div className={styles['guaguale']}>
+
+      </div>
     </div>
-  )
+  );
 }
 
 export const metadata: Metadata = {
   title: 'Transition Css',
-}
- 
+};
