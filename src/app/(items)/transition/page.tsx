443648fcfddb8b2877753ac287c6037f20740c81
XyanZhang <1743067175@qq.com>
2023-11-26 22:24:07 +0800

    feat: transition 实现刮刮乐效果

--- src/app/(items)/transition/page.tsx
+++ src/app/(items)/transition/page.tsx
@@ -7,19 +7,27 @@ export default function TransitionCSS() {
       <div className="flex justify-around h-40 items-center">
         <div className={styles['g-container']}>
           <div className={styles['g-button']}>
-              Button
-              <ul>
-                  <li>>> Lorem, ipsum dolor sit amet?</li>
-                  <li>>> Lorem, ipsum dolor sit amet?</li>
-                  <li>>> Lorem, ipsum dolor sit amet?</li>
-                  <li>>> Lorem, ipsum dolor sit amet?</li>
-              </ul>
+            Button
+            <ul>
+              <li> Lorem, ipsum dolor sit amet?</li>
+              <li> Lorem, ipsum dolor sit amet?</li>
+              <li> Lorem, ipsum dolor sit amet?</li>
+              <li> Lorem, ipsum dolor sit amet?</li>
+            </ul>
           </div>
         </div>
         <div className={styles['hover-trans']}>按钮hover</div>
       </div>
       <div className={styles['guaguale']}>
-
+        <div className={styles["g-box"]}>
+          {new Array(30).fill(1).map((row, i) => (
+            <div key={i} className={styles["g-row"]}>
+              {new Array(30).fill(2).map((item, index) => (
+                <div key={index} className={styles["g-item"]}></div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
